fd418cda9eb8e7fa8c772cb1919d8765cbcb01e2
Cvieira <dullvieira@gmail.com>
2024-02-07 19:29:56 +0000

    [feature] vessel route list: add virtual scroll

--- modules/map/src/lib/map/ui/vessel-route-list/vessel-route-list.component.ts
+++ modules/map/src/lib/map/ui/vessel-route-list/vessel-route-list.component.ts
@@ -11,13 +11,14 @@ import { MatListModule } from '@angular/material/list';
 import { VesselRoute } from '../../models/vessel-route.model';
 import { Observable } from 'rxjs';
 import { VesselsService } from '../../data-access/vessels.service';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'vsm-vessel-route-list',
   templateUrl: './vessel-route-list.component.html',
   styleUrls: ['./vessel-route-list.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatListModule],
+  imports: [CommonModule, MatListModule, ScrollingModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VesselRouteListComponent implements OnInit {
